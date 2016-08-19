30bb42469b876a3698c94a1890e55505c5d6790c
Yaroslav Zemlyanuhin <y.zemlyanuhin@cti.ru>
2016-08-19 18:04:35 +0300

    Added Exercise 2: Sentiment Analysis on movie reviews

--- exercise_02_sentiment.py
+++ exercise_02_sentiment.py
@@ -20,8 +20,7 @@ from sklearn.datasets import load_files
 from sklearn.cross_validation import train_test_split
 from sklearn import metrics
 
-
-if __name__ == "__main__":
+def main(argv):
     # NOTE: we put the following in a 'if __name__ == "__main__"' protected
     # block to be able to use a multi-core grid search that also works under
     # Windows, see: http://docs.python.org/library/multiprocessing.html#windows
@@ -29,9 +28,9 @@ if __name__ == "__main__":
     # that is used when n_jobs != 1 in GridSearchCV
 
     # the training data folder must be passed as first argument
-    movie_reviews_data_folder = sys.argv[1]
+    movie_reviews_data_folder = argv[1]
     dataset = load_files(movie_reviews_data_folder, shuffle=False)
-    print("n_samples: %d" % len(dataset.data))
+    print "n_samples: %d" % len(dataset.data)
 
     # split the dataset in training and test set:
     docs_train, docs_test, y_train, y_test = train_test_split(
@@ -39,25 +38,49 @@ if __name__ == "__main__":
 
     # TASK: Build a vectorizer / classifier pipeline that filters out tokens
     # that are too rare or too frequent
+    vectorizer = TfidfVectorizer()
+
 
     # TASK: Build a grid search to find out whether unigrams or bigrams are
     # more useful.
+    # # from sklearn.grid_search import GridSearchCV
+    parameters = {
+        'vect__ngram_range': [(1, 1), (1, 2), (1, 3)],
+        'vect__max_df': [0.9, 0.95],
+        'vect__min_df': [0.05, 3],
+    }
+
     # Fit the pipeline on the training set using grid search for the parameters
+    clf = Pipeline([
+        ('vect', vectorizer),
+        ('clf', LinearSVC(C=1000)),
+    ])
+
+    gs_clf = GridSearchCV(clf, parameters, n_jobs=-1)
+    gs_clf = gs_clf.fit(docs_train, y_train)
+
 
     # TASK: print the cross-validated scores for the each parameters set
     # explored by the grid search
+    best_parameters, score, _ = max(gs_clf.grid_scores_, key=lambda x: x[1])
+    for param_name in sorted(parameters.keys()):
+        print "%s: %r" % (param_name, best_parameters[param_name])
 
+    print "score: %s" % score
     # TASK: Predict the outcome on the testing set and store it in a variable
     # named y_predicted
 
+    y_predicted = gs_clf.predict(docs_test)
     # Print the classification report
-    print(metrics.classification_report(y_test, y_predicted,
-                                        target_names=dataset.target_names))
+    print metrics.classification_report(y_test, y_predicted,
+                                        target_names=dataset.target_names)
 
     # Print and plot the confusion matrix
-    cm = metrics.confusion_matrix(y_test, y_predicted)
-    print(cm)
-
+    conf_matrix = metrics.confusion_matrix(y_test, y_predicted)
+    print conf_matrix
     # import matplotlib.pyplot as plt
     # plt.matshow(cm)
     # plt.show()
+
+if __name__ == "__main__":
+    main(sys.argv)
